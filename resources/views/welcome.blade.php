c66abfcc991810b5db5bd07f5588d34d88fb9c4a
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-04-05 16:56:25 +0700

    Added app engine configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Practicing Devops
                 </div>
 
                 <div class="links">
