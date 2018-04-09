eec16308ef432fc5792f21104c6972e71cfefb93
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2018-04-09 14:24:05 +0700

    sample throw exception to test GCP StackDriver connection

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test-exception', function() {
+    \Log::error("Going to throw an exception.");
+    throw new \Exception("Success throw exception.");
+});
